24cfc6c766e0adac8766bcc13ec5df6776ed4952
Moses Hunsu <hunsumoses7@gmail.com>
2023-06-19 18:53:17 +0100

    Rendered Lists

--- src/index.js
+++ src/index.js
@@ -71,22 +71,12 @@ function Header() {
 function Menu() {
   return (
     <main className="menu">
-      <div>
-        <h2>Our menu</h2>
-        <Pizza
-          name="Pizza spinaci"
-          ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-          photoName="pizzas/spinaci.jpg"
-          price={10}
-        />
-
-        <Pizza
-          name="Pizza Funghi"
-          photoName="pizzas/funghi.jpg"
-          price={12}
-          ingredients="Tomato, mozarella, mushrooms, and onion"
-        />
-      </div>
+      <h2>Our menu</h2>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
     </main>
   );
 }
@@ -95,14 +85,14 @@ function Pizza(props) {
   console.log(props);
 
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
